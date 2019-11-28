94bf565adb1abbb545701e54a5c0064fead1fb8b
Reduichi <q2339198@gmail.com>
2019-11-28 21:28:00 +0800

    作業4-1

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("血量")]
+    public int Hp = 100;
+    [Header("攻擊力")]
+    public int Atk;
+    [Header("殭屍")]
+    public GameObject Zb;
+    [Header("喇叭")]
+    public GameObject Aud;
+    [Header("音效")]
+    public GameObject SoundAtk;
+
+
+}
---
+++ UnityProject/Assets/Zombie.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [Header("血量")]
+    public int Hp = 50;
+    [Header("攻擊力")]
+    public int Atk;
+    [Header("玩家")]
+    public GameObject Player;
+}
