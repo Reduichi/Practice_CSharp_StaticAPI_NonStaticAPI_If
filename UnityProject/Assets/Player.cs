dd85e75a07d4cb5d36794d0506b83f8d6399b5fa
Reduichi <q2339198@gmail.com>
2019-12-05 10:47:12 +0800

    作業4完成

--- UnityProject/Assets/Player.cs
+++ UnityProject/Assets/Player.cs
@@ -14,27 +14,28 @@ public class Player : MonoBehaviour
     public AudioClip SoundAtk;
 
 
-    public void Attake() // 玩家的攻擊
+    public void Attake() // 玩家的攻擊與儲存數值
     {
         if (Input.GetKeyDown("a"))
         {
             Atk = Random.Range(10f, 20f);
-            print("殭屍受到的傷害: " + Atk);
-            Zb.Hurt(Atk);
+            print("<color=blue>殭屍受到的傷害: " + Atk + Hp + "</color>");
+            Zb.Hurt(Atk);  // 將Atk的數值存到Zombie中的Hurt方法
+            Aud.PlayOneShot(SoundAtk, 1f);  //開啟音效
         }
     }
 
-    public void Hurt(float damage) // 玩家的受傷
+    public void Hurt(float damage) // 玩家的受傷與剩餘血量
     {
-        Hp = Hp - damage;
-        print("玩家剩下的血量: " + Hp);
+        Hp = Hp - damage;   // damage用來讀取Zombie的攻擊數值
+        print("<color=red>玩家剩下的血量: " + Hp + "</color>");
     }
 
     public void Dead() // 玩家的死亡
     {
         if (Hp <= 0)
         {
-            print("玩家死亡");
+            print("<color=red>玩家死亡</color>");
         }
     }
 
--- UnityProject/Assets/Zombie.cs
+++ UnityProject/Assets/Zombie.cs
@@ -5,7 +5,7 @@ public class Zombie : MonoBehaviour
     [Header("血量")]
     public float Hp = 50;
     [Header("攻擊力")]
-    public float Atk = Random.Range(20f, 30f);
+    public float Atk;
     [Header("玩家")]
     public Player Player;  // 可以使用Player的參數
     [Header("喇叭")]
@@ -13,27 +13,28 @@ public class Zombie : MonoBehaviour
     [Header("音效")]
     public AudioClip SoundAtk;
 
-    public void Attake()   // 殭屍的攻擊
+    public void Attake()   // 殭屍的攻擊與儲存數值
     {
         if (Input.GetKeyDown("b"))
         {
             Atk = Random.Range(20f, 30f);
-            print("玩家受到的傷害: " + Atk);
-            Player.Hurt(Atk);
+            print("<color=red>玩家受到的傷害: " + Atk + "</color>");
+            Player.Hurt(Atk);  // 將Atk的數值存到Player中的Hurt方法
+            Aud.PlayOneShot(SoundAtk, 1f);  //開啟音效
         }
     }
 
-    public void Hurt(float damage)  // 殭屍的受傷
+    public void Hurt(float damage)  // 殭屍的受傷與剩餘的血量
     {
-        Hp = Hp - damage;
-        print("殭屍剩下的血量: " + Hp);
+        Hp = Hp - damage;  // damage用來讀取Player的攻擊數值
+        print("<color=blue>殭屍剩下的血量: " + Hp + "</color>");
     }
 
     public void Dead()  // 殭屍的死亡
     {
         if (Hp <= 0)
         {
-            print("殭屍死亡");
+            print("<color=blue>殭屍死亡</color>");
         }
     }
 
